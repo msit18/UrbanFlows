392de28df2733850eb298262dfc7be7c4a638069
Michelle Sit <msit@wellesley.edu>
2015-08-28 17:40:16 -0400

    cleaned up some files

--- slavePi2/attempt.py
+++
@@ -1,5 +0,0 @@
-import os
-
-print os.stat("image.data")
-with open ("image.data", "rb") as f:
-	print repr(f.read())
--- slavePi2/restartWifi.sh
+++
@@ -1,4 +0,0 @@
-sudo ifdown eth0
-sudo ifdown wlan0
-sudo ifup wlan0
-sudo ifup eth0
