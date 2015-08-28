f4843757a42372d87db1ba931cf8a7f351272999
Michelle Sit <msit@wellesley.edu>
2015-08-28 17:31:21 -0400

    cleaned up files

--- slavePi2/pastFiles/takeVideo.sh
+++ slavePi2/pastFiles/takeVideo.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-#Written by Michelle Sit
-#Takes a video and sends it to the masterPi
-
 DATE=$(date "+%Y_%m_%d_%H_%M_%2S_%3N")
 echo;
 
-raspivid -o /home/pi/$DATE.h264 -t 60000 -w 640 -h 480;
+raspivid -o /home/pi/$DATE.h264 -t 10000 -w 1920 -h 1080;
 
 sshpass -p 'raspberry' scp /home/pi/$DATE.h264 pi@10.0.0.1:/home/pi/;
+
+sshpass -p 'raspberry' ssh pi@10.0.0.1 python flash.py fin
 
 done
---
+++ slavePi2/pastFiles/writeYUVPixelsToTextFile2.py
@@ -0,0 +1,37 @@
+#Attempting to write the pixel numbers of a yuv image to a text file
+#Could be a successful file.  Needs to be retested
+
+#Written by Michelle Sit
+
+from picamera import PiCamera
+import time
+from picamera.array import PiYUVArray
+
+import numpy as np
+
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiYUVArray(camera, size=(640, 480) )
+
+for frame in camera.capture_continuous(rawCapture, format="yuv", use_video_port=True):
+	image = frame.array
+	print image
+	with file('test.txt', 'w') as outfile:
+		outfile.write('#Array shape:{0}\n'.format(image.shape) )
+		for data_slice in image:
+			np.savetxt(outfile, data_slice, fmt='%-7.2f')
+			outfile.write('# New slice\n')
+	rawCapture.truncate(0)
+	print "rawCapture truncate"
+	exit()
+print "last exit"
+exit()
+#with picamera.PiCamera() as camera:
+#	camera.resolution = (640, 480)
+#	camera.resolution = (1920, 1080)
+#	start = time.time()
+#	camera.capture_sequence('image.csv', format="raw")
+#	finish = time.time()
+#	final = finish-start
+#	print final
