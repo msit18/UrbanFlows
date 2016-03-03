a6496f1b2839e6a3bfcc26ce9804faec2de84a99
Michelle Sit <msit@wellesley.edu>
2016-03-02 19:31:57 -0500

    trying to figure out how to implement the different picture, video, mplxr modes

--- slavePi2/testUploadSpeed2.py
+++ slavePi2/testUploadSpeed2.py
@@ -11,7 +11,7 @@ import subprocess
 #from twisted.web.client import FileBodyProducer
 
 #Threading for picture transfer
-#from manualPic_capturePhotos import takePictures
+from manualPic_capturePhotos2 import takePictureClass
 import os
 import glob
 import time
@@ -54,7 +54,7 @@ class myProtocol(protocol.Protocol):
 		self.name = ""
 		self.clientParams = ""
 		self.numPicsTaken = 0
-		self.runSendImg = True
+		#self.runSendImg = True
 
 	def connectionMade(self):
 		#ip_address = subprocess.check_output("hostname --all-ip-addresses", shell=True).strip()
@@ -71,9 +71,11 @@ class myProtocol(protocol.Protocol):
 			#TAKING PICTURES IN SEPARATE THREAD. Has errback handling here
 			if msgFromServer[2] == "camera":
 				print "this is a camera command"
+				global tp
+				tp.takePictureClass()
 				startAtTime = self.calculateTimeDifference(msgFromServer[9], msgFromServer[10])
 				callLaterTimeCollectImgs = startAtTime + 1
-				result = threads.deferToThread(self.takePicture, int(msgFromServer[3]), int(msgFromServer[4]),\
+				result = threads.deferToThread(tp.takePicture, int(msgFromServer[3]), int(msgFromServer[4]),\
 					int(msgFromServer[5]), int(msgFromServer[6]), int(msgFromServer[7]), int(msgFromServer[8]), startAtTime)
 			elif msgFromServer[2] == "video":
 				print "this is the video method. Video method has not been completed"
@@ -101,76 +103,9 @@ class myProtocol(protocol.Protocol):
 
 #WOULD BE FUN TODO: REPLACE WHILE LOOP WITH PRINTING UPDATES ON FILE TO A GRAPH APPROACH.
 #HAVE THE FPS UPDATED AT A CERTAIN TIME FRAME ON A GRAPH IF POSSIBLE.
-	def takePicture (self, inputTotalTime, inputResW, inputResH, inputNumPics, inputFPSTimeInterval, inputFramerate, inputStartTime):
-		print "takePicture method!"
-		while time.time() < inputStartTime:
-			pass
-		else:
-			try:
-				#Keeps track of time for updates 
-				prgmStartTime = time.time() #When the program began
-				totalTimeSec = int(inputTotalTime)
-				totalTimeMin = int(inputTotalTime)/60
-				timeNow = time.time() #Used to keep track of current time
-				prgmEndTime = totalTimeSec+timeNow #When the program ends
-				timePlusFPSTimeInterval = timeNow #Keeps track of time increments
-
-				timePlusTenMin = timeNow+5
-				# print "Capturing {0}p for a total time of {1} min ({2} secs) at {3} "\
-				# "frames per {4} second (({5} mins) at {6} framerate ".format(str(resH), \
-				# 	str(totalTimeMin), str(totalTimeSec), str(numPics), str(timeInterval), \
-				# 	str(float(timeInterval/60)), str(frameRate) )
-				totalNumPicsTaken = 0
-				#Limits program from going over the designated time period and over the update time
-				while timeNow < timePlusTenMin and timeNow < prgmEndTime:
-					timeNow = time.time()
-					#Provides updates in 10 minute increments
-					if timeNow >= timePlusTenMin:
-						endTenMinTime = time.time()
-						tenMinTotalRunTime = endTenMinTime-prgmStartTime
-						tenMinFPSUpdate = totalNumPicsTaken/tenMinTotalRunTime
-						#print "10.2 Ten Min Update: Total number of pictures is {0},"\
-						#" total time elapsed is {1}, totalFPS is {2}".format(str(totalNumPicsTaken),\
-						# str(tenMinTotalRunTime), str(tenMinFPSUpdate) )
-						timePlusTenMin = time.time()+5
-					else: #Runs picture taking process as normal
-						while timeNow > timePlusFPSTimeInterval:
-							timePlusFPSTimeInterval = timeNow + inputFPSTimeInterval
-							start = time.time()
-							self.piCamTakePictures(inputResW, inputResH, inputNumPics, inputFramerate)
-							finish = time.time()
-							#Analyzing time and frames
-							fpsTime = (finish-start)
-							fps = inputNumPics/fpsTime
-							totalNumPicsTaken += inputNumPics
-							#print 'Captured {0} frames at {1}fps in {2}secs'\
-							#.format(str(totalNumPicsTaken), str(inputNumPics/fpsTime), str(fpsTime))
-				endTime = time.time()
-				totalTime = endTime-prgmStartTime
-				totalFPS = totalNumPicsTaken/totalTime
-				#print "10.2: Captured {0} total pictures.  Total time was {1}, total FPS is {2}"\
-				#.format(str(totalNumPicsTaken), str(inputTotalTime), str(totalFPS) )
-				print "CAMERA IS FINISHED. RETURN FALSE"
-				self.runSendImg = False
-			except:
-				print "noooooooooooooo break"
-				print sys.exc_info()[0]
-				raise
-
-	def piCamTakePictures(self, inputResW, inputResH, inputNumPics, inputFramerate):
-		with picamera.PiCamera() as camera:
-			camera.resolution = (inputResW, inputResH)
-			camera.framerate = inputFramerate
-			v = camera.capture_sequence([
-				datetime.datetime.now().strftime ('%M_%S_%f') + '.jpg'
-				# datetime.datetime.now().strftime ('%d-%m-%Y-%H_%M_%S_%f') + '_TT'\
-				#  + str(listServerArgs[0]) + '_RES' + str(resH) + '_PIC' + str(numPics) +\
-				#   '_TI' + str(timeInterval) + '_FR' + str(frameRate) + '.jpg'
-				for i in range(inputNumPics)
-				], use_video_port=True)
 
 	def getRunSendImgMethod(self):
-		return self.runSendImg
+		return tp.runSendImg
 
 	def curlUploadImg (self):
 		self.fileList = glob.glob('*.jpg')
@@ -184,16 +119,17 @@ class myProtocol(protocol.Protocol):
 			pass
 		else:
 			print "sendImages method!"
-			while self.runSendImg == True:
+			while tp.runSendImg == True:
 				self.curlUploadImg()
 				print self.getRunSendImgMethod()
-			else: #if self.runSendImg is False
+			else: #if tp.runSendImg is False
 				print "runing last glob"
 				self.curlUploadImg()	
 				print "done! :D"
 
 if __name__ == '__main__':
 	jobs = DeferredQueue()
+	#tp = takePictureClass()
 
 	#TCP network: Connects on port 8888. HTTP network: Connects on port 8880
 	data = "start"
