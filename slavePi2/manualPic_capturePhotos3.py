405e97bdb48b29b9f6d3d0ea64588c2e7305e1c4
Michelle Sit <msit@wellesley.edu>
2016-05-09 18:59:59 -0400

    found method for faster fps

--- slavePi2/manualPic_capturePhotos3.py
+++ slavePi2/manualPic_capturePhotos3.py
@@ -18,24 +18,37 @@ class takePictureClass():
 	def __init__(self):
 		self.runUpload = True
 
-	def takePicture_rs (self, inputTotalTime, inputResW, inputResH,\
-							inputFPSTimeInterval, inputStartTime):
+	def filenames(self, inputTotalTime):
+		print "took some images"
+		nowPlusInputTotalTime = time.time() + inputTotalTime + 2
+		while time.time() < nowPlusInputTotalTime:
+			yield datetime.datetime.now().strftime ('%M_%S_%f') + '.jpg'
+
+	def takePicture_filenames(self, inputTotalTime, inputResW, inputResH,\
+							inputFramerate, inputStartTime):
 		while time.time() < inputStartTime:
 			pass
 		else:
-			try:
-				print "subprocess call raspistill timelapse function"
-				subprocess.call('raspistill -o {0}.jpg -t {1} -tl {2} -w {3} -h {4}'\
-					.format(datetime.datetime.now().strftime ('%M_%S_%f'),\
-					 totalTimeMilliSec, inputFPSTimeInterval, inputResW, inputResH))
-				print "CAMERA IS FINISHED. RETURN FALSE"
-				self.runUpload = False
-
-			except:
-				print "noooooooooooooo break"
-				self.runUpload = False
-				print "Switched runUpload"
-				raise
+			print "running"
+			# try:
+			with picamera.PiCamera() as camera:
+			    camera.resolution = (inputResW, inputResH)
+			    camera.framerate = inputFramerate
+			    camera.start_preview()
+			    # Give the camera some warm-up time
+			    time.sleep(2)
+			    start = time.time()
+			    camera.capture_sequence(self.filenames(inputTotalTime), use_video_port=True)
+			    finish = time.time()
+			    frames = len(glob.glob('*.jpg'))
+			print('Captured %d frames at %.2ffps' % (frames, frames / (finish - start)))
+			self.runUpload = False
+			print "runUpload is false. Finished"
+			# except:
+			# 	print "noooooooooooooo break"
+			# 	self.runUpload = False
+			# 	print "Switched runUpload"
+			# 	raise
 
 	def takePicture_cc (self, inputTotalTime, inputResW, inputResH,\
 							inputNumPics, inputFPSTimeInterval, inputStartTime):
@@ -74,65 +87,65 @@ class takePictureClass():
 				print "Switched runUpload"
 				raise
 
-	def takePicture (self, inputTotalTime, inputResW, inputResH, inputNumPics,\
-						inputFPSTimeInterval, inputFramerate, inputStartTime):
-		#print "takePicture method!"
-		while time.time() < inputStartTime:
-			pass
-		else:
-			try:
-				#Keeps track of time for updates 
-				prgmStartTime = time.time() #When the program began
-				totalTimeSec = int(inputTotalTime)
-				totalTimeMin = int(inputTotalTime)/60
-				timeNow = time.time() #Used to keep track of current time
-				prgmEndTime = totalTimeSec+timeNow #When the program ends
-				timePlusFPSTimeInterval = timeNow #Keeps track of time increments
+	# def takePicture (self, inputTotalTime, inputResW, inputResH, inputNumPics,\
+	# 					inputFPSTimeInterval, inputFramerate, inputStartTime):
+	# 	#print "takePicture method!"
+	# 	while time.time() < inputStartTime:
+	# 		pass
+	# 	else:
+	# 		try:
+	# 			#Keeps track of time for updates 
+	# 			prgmStartTime = time.time() #When the program began
+	# 			totalTimeSec = int(inputTotalTime)
+	# 			totalTimeMin = int(inputTotalTime)/60
+	# 			timeNow = time.time() #Used to keep track of current time
+	# 			prgmEndTime = totalTimeSec+timeNow #When the program ends
+	# 			timePlusFPSTimeInterval = timeNow #Keeps track of time increments
 
-				timePlusTenMin = timeNow+5
-				# print "Capturing {0}p for a total time of {1} min ({2} secs) at {3} "\
-				# "frames per {4} second (({5} mins) at {6} framerate ".format(str(resH), \
-				# 	str(totalTimeMin), str(totalTimeSec), str(numPics), str(timeInterval), \
-				# 	str(float(timeInterval/60)), str(frameRate) )
-				totalNumPicsTaken = 0
-				#Limits program from going over the designated time period and over the update time
-				while timeNow < timePlusTenMin and timeNow < prgmEndTime:
-					timeNow = time.time()
-					#Provides updates in 10 minute increments
-					timePlusFPSTimeInterval = timeNow + inputFPSTimeInterval
-					start = time.time()
-					self.piCamTakePictures(inputResW, inputResH, inputNumPics, inputFramerate)
-					finish = time.time()
-					#Analyzing time and frames
-					fpsTime = (finish-start)
-					fps = inputNumPics/fpsTime
-					totalNumPicsTaken += inputNumPics
-					# print 'Captured {0} frames at {1}fps in {2}secs'\
-					# .format(str(totalNumPicsTaken), str(inputNumPics/fpsTime), str(fpsTime))
-				endTime = time.time()
-				totalTime = endTime-prgmStartTime
-				totalFPS = totalNumPicsTaken/totalTime
-				# print "10.2: Captured {0} total pictures.  Total time was {1}, total FPS is {2}"\
-				# .format(str(totalNumPicsTaken), str(inputTotalTime), str(totalFPS) )
-				print "CAMERA IS FINISHED. RETURN FALSE"
-				self.runUpload = False
-			except:
-				print "noooooooooooooo break"
-				self.runUpload = False
-				print "Switched runUpload"
-				raise
+	# 			timePlusTenMin = timeNow+5
+	# 			# print "Capturing {0}p for a total time of {1} min ({2} secs) at {3} "\
+	# 			# "frames per {4} second (({5} mins) at {6} framerate ".format(str(resH), \
+	# 			# 	str(totalTimeMin), str(totalTimeSec), str(numPics), str(timeInterval), \
+	# 			# 	str(float(timeInterval/60)), str(frameRate) )
+	# 			totalNumPicsTaken = 0
+	# 			#Limits program from going over the designated time period and over the update time
+	# 			while timeNow < timePlusTenMin and timeNow < prgmEndTime:
+	# 				timeNow = time.time()
+	# 				#Provides updates in 10 minute increments
+	# 				timePlusFPSTimeInterval = timeNow + inputFPSTimeInterval
+	# 				start = time.time()
+	# 				self.piCamTakePictures(inputResW, inputResH, inputNumPics, inputFramerate)
+	# 				finish = time.time()
+	# 				#Analyzing time and frames
+	# 				fpsTime = (finish-start)
+	# 				fps = inputNumPics/fpsTime
+	# 				totalNumPicsTaken += inputNumPics
+	# 				# print 'Captured {0} frames at {1}fps in {2}secs'\
+	# 				# .format(str(totalNumPicsTaken), str(inputNumPics/fpsTime), str(fpsTime))
+	# 			endTime = time.time()
+	# 			totalTime = endTime-prgmStartTime
+	# 			totalFPS = totalNumPicsTaken/totalTime
+	# 			# print "10.2: Captured {0} total pictures.  Total time was {1}, total FPS is {2}"\
+	# 			# .format(str(totalNumPicsTaken), str(inputTotalTime), str(totalFPS) )
+	# 			print "CAMERA IS FINISHED. RETURN FALSE"
+	# 			self.runUpload = False
+	# 		except:
+	# 			print "noooooooooooooo break"
+	# 			self.runUpload = False
+	# 			print "Switched runUpload"
+	# 			raise
 
-	def piCamTakePictures(self, inputResW, inputResH, inputNumPics, inputFramerate):
-		with picamera.PiCamera() as camera:
-			camera.resolution = (inputResW, inputResH)
-			camera.framerate = inputFramerate
-			v = camera.capture_sequence([
-				datetime.datetime.now().strftime ('%M_%S_%f') + '.jpg'
-				# datetime.datetime.now().strftime ('%d-%m-%Y-%H_%M_%S_%f') + '_TT'\
-				#  + str(listServerArgs[0]) + '_RES' + str(resH) + '_PIC' + str(numPics) +\
-				#   '_TI' + str(timeInterval) + '_FR' + str(frameRate) + '.jpg'
-				for i in range(inputNumPics)
-				], use_video_port=False)
+	# def piCamTakePictures(self, inputResW, inputResH, inputNumPics, inputFramerate):
+	# 	with picamera.PiCamera() as camera:
+	# 		camera.resolution = (inputResW, inputResH)
+	# 		camera.framerate = inputFramerate
+	# 		v = camera.capture_sequence([
+	# 			datetime.datetime.now().strftime ('%M_%S_%f') + '.jpg'
+	# 			# datetime.datetime.now().strftime ('%d-%m-%Y-%H_%M_%S_%f') + '_TT'\
+	# 			#  + str(listServerArgs[0]) + '_RES' + str(resH) + '_PIC' + str(numPics) +\
+	# 			#   '_TI' + str(timeInterval) + '_FR' + str(frameRate) + '.jpg'
+	# 			for i in range(inputNumPics)
+	# 			], use_video_port=False)
 
 	def curlUpload (self, serverIP):
 		print "curlUploadImg called"
@@ -148,11 +161,8 @@ class takePictureClass():
 					' wait;'\
 					' else sudo ifup wlan0;'\
 					' fi'.format(serverIP, item)
-				print "cmd: ", cmd
+				#print "cmd: ", cmd
 				subprocess.call(cmd, shell=True)
-
-	def curlUploadErrback(self):
-		return "Error!!"
 
 	def sendUpload(self, inputStartTimePlusOne, serverIP):
 		while time.time() < inputStartTimePlusOne:
@@ -171,7 +181,7 @@ class takePictureClass():
 							' wait;'\
 							' else sudo ifup wlan0;'\
 							' fi'.format(serverIP, item)
-						print "cmd: ", cmd
+						#print "cmd: ", cmd
 						subprocess.call(cmd, shell=True)
 				print "sleeping for 1 seconds"
 				time.sleep(1)
@@ -185,6 +195,6 @@ class takePictureClass():
 
 if __name__ == '__main__':
 	t = takePictureClass()
-	now = time.time()+1
-	t.takePicture_cc(10, 2592, 1944, 10, 1, now)
+	#now = time.time()+1
+	#t.takePicture_cc(10, 2592, 1944, 10, 1, now)
 	#camLog = open('CamLog-{0}.txt'.format(time.strftime("%Y-%m-%d-%H:%M:%S")), 'w')
--- slavePi2/testCamera.py
+++ slavePi2/testCamera.py
@@ -34,7 +34,57 @@ def runStuff():
 		print "lols"
 		yield 'image_{0}.jpg'.format(i)
 
-callbackImg()
+def takePictures():
+	with picamera.PiCamera() as camera:
+		camera.resolution = (1280, 720)
+		camera.framerate = 30
+		start = time.time()
+		#camera.capture_sequence(['image%02d.jpg' % i for i in range(10)])
+		end = time.time()
+		totalTime = end-start
+		fps = 10/totalTime
+		print "Captured 10 images in {0} in {1} fps".format(totalTime, fps)
+
+def takePictures2():
+	start = time.time()
+	camera = picamera.PiCamera(sensor_mode=4)
+	camera.framerate = 35
+	camera.capture_sequence(['image%02d.jpg' % i for i in range(10)], use_video_port=False)
+	end = time.time()
+	totalTime = end-start
+	fps = 10/totalTime
+	print "Captured 10 images in {0} in {1} fps".format(totalTime, fps)
+
+frames = 10
+
+def filenames2():
+    frame = 0
+    while frame < frames:
+        yield datetime.datetime.now().strftime ('%M_%S_%f')+'.jpg'
+        frame += 1
+
+def filenames():
+    nowPlusTen = time.time() + 10
+    while time.time() < nowPlusTen:
+        yield datetime.datetime.now().strftime ('%M_%S_%f')+'.jpg'
+        #frame += 1
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (2592,1944)
+    camera.framerate = 7
+    camera.start_preview()
+    # Give the camera some warm-up time
+    time.sleep(2)
+    start = time.time()
+    camera.capture_sequence(filenames(), use_video_port=True)
+    finish = time.time()
+    frames = len(glob.glob('*.jpg'))
+print('Captured %d frames at %.2ffps' % (
+    frames,
+    frames / (finish - start)))
+
+# takePictures2()
+#callbackImg()
 # serverIP = '18.189.71.62'
 # fileList = glob.glob('*.jpg')
 # for img in fileList:
