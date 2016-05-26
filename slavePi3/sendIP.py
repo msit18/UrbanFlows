be7ff45e9889402d9abb2b3f8b9b2c50ca5f1063
Michelle Sit <msit@wellesley.edu>
2016-05-26 12:09:07 -0400

    minor edits to preview on SlavePi3

--- slavePi3/sendIP.py
+++ slavePi3/sendIP.py
@@ -1,4 +1,4 @@
 import subprocess, os
 
 ip_address = subprocess.check_output("hostname --all-ip-addresses", shell=True).strip()
-os.system('echo {0} | mail -s "Pi1 IP Address" msit@wellesley.edu'.format(ip_address))
+os.system('echo {0} | mail -s "Pi3 IP Address" msit@wellesley.edu'.format(ip_address))
