2adaae4ba3c44d3276fb7d6cc062cf21c474cde1
Pi9 <urbanflowsproject@gmail.com>
2017-02-03 17:19:33 -0500

    Updated client side. Use for deployment

---
+++ slavePi/emailIPAddr.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+_IP=$(hostname -I)
+echo "CRON pi address for 10.0.0.9 is $(hostname -I)" | mail -s "Pi9 address" urbanflowsproject@gmail.com
+
+
+
+
+#IPAddr = Log IP address
+
+#If the IP addr changes, send email
+#if IPAddr != (call IP addr):
+#	IPAddr = (call IP addr)
+
+#If wifi is down. Send email when it gets back up IF IPaddr is different
