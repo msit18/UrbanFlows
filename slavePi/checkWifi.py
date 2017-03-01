de74b5b8b2e586f937dbe68ee8915b3801ffbef5
msit18 <msit@mit.edu>
2017-03-01 12:43:18 -0500

    working wifi

--- slavePi/checkWifi.py
+++ slavePi/checkWifi.py
@@ -1,4 +1,4 @@
-import subprocess, datetime
+import subprocess, datetime, time
 
 def fixWifi():
 	writeFile("Running check {0}".format(datetime.datetime.now()))
@@ -7,7 +7,8 @@ def fixWifi():
 		checkWifiDown = subprocess.call("[\"$(/bin/ping -c 3 8.8.8.8)\"]", shell=True)
 		writeFile("checkWifiDown " + str(checkWifiDown))
 	except subprocess.CalledProcessError, e:
-	    writeFile("Ping stdout output:\n", e.output)
+		writeFile("Ping stdout output:\n" + e.output)
+		checkWifiDown = 0
 
 	if int(checkWifiDown) == 2:
 		writeFile("Wifi is working")
@@ -17,7 +18,7 @@ def fixWifi():
 		while restartWifiTries < 4:
 			writeFile("---------------Num times tried to restart wifi: {0}/3".format(restartWifiTries))
 			_checkWifiDown = restartWifi()
-			writeFile("_checkWifiDown second time: ", str(_checkWifiDown))
+			writeFile("_checkWifiDown second time: " + str(_checkWifiDown))
 			if int(_checkWifiDown) == 2:
 				writeFile("Reconnected successfully. Connecting to server again.")
 				break
@@ -31,7 +32,10 @@ def restartWifi():
 	subprocess.call("sudo ifdown eth0; sudo ifdown wlan0; sudo ifup wlan0; sudo ifup eth0", shell=True)
 	writeFile("sleeping...")
 	time.sleep(10)
-	writeFile(subprocess.check_output(["ping", "-c", "3", "8.8.8.8"]))
+	try:
+		writeFile(subprocess.check_output(["ping", "-c", "3", "8.8.8.8"]))
+	except subprocess.CalledProcessError, e:
+	    writeFile("Ping stdout output:\n" + e.output)
 	return subprocess.call("[\"$(/bin/ping -c 3 8.8.8.8)\"]", shell=True)
 
 def writeFile(msg):
